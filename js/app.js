bf2983e02d2abe9034cd1db6d1a80f644d3baff5
Tsotne Chaduneli <86349055+tsotne-chaduneli@users.noreply.github.com>
2024-03-29 00:50:55 +0400

    Updated new line

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// Added new line
+// Updated new line
 const workspace = document.querySelector('#workspace')
 const contextmenu = document.querySelector('#contextmenu')
 const newFile = document.querySelector('#contextmenu .new-file')
